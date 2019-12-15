a24a5ce0d2fc08043bbc4a20ec20787024c7efb1
Chris Castellano <chris.castellano621@Gmail.com>
2019-12-14 20:02:24 -0500

    memo fix 3

--- src/com/helen/commands/Command.java
+++ src/com/helen/commands/Command.java
@@ -324,7 +324,7 @@ public class Command {
 	@IRCCommand(command = {".rem"}, startOfLine = true, coexistWithJarvis = true, securityLevel = 1)
 	public void remember(CommandData data){
 		if(Configs.getProperty("remchannels").stream().anyMatch(config -> config.getValue().equalsIgnoreCase(data.getChannel()))){
-			helen.sendMessage(data.getResponseTarget(), data.getSender() + ": " + addMemo(data.getSplitMessage()[1], data.getMessageWithoutCommand()));
+			helen.sendMessage(data.getResponseTarget(), data.getSender() + ": " + addMemo(data.getSplitMessage()[1], data.getMessageWithoutCommand().substring(data.getMessageWithoutCommand().split(" ")[0].length() + 1)));
 		}
 	}
 
